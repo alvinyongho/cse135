f0db55067d9e93e98b1363c104f7c7b1dd9950b9
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-11 10:24:36 -0700

    update cgi script

--- hw2/hello.pl
+++ hw2/hello.pl
@@ -1,5 +1,3 @@
 #!/usr/bin/perl
 print "Content-type: text/html\n\n";
 print "Hello world from cgi which is not another name for perl although it is written in perl... anyways, the time is \n";
-my $now_string = localtime;
-print $now_string;
