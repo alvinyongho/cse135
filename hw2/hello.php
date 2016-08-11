2521d318e0cbc2d500c5b1ab0baf5d91a47bb24f
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-11 11:20:31 -0700

    add exec rights to scrips

--- hw2/hello.php
+++ hw2/hello.php
@@ -8,6 +8,10 @@
 <?php
 	echo '<p>Hello World from PHP on ' . date('Y-m-d h:i:s a') . '</p>';
 
+	function random_color_part() {
+    return str_pad( dechex( mt_rand( 0, 255 ) ), 2, '0', STR_PAD_LEFT);
+}
+
 	function random_color() {
     return random_color_part() . random_color_part() . random_color_part();
 }
