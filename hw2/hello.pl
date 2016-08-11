070a0aad7037e931074d8565cff80ed3e23ca4f5
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-11 11:02:01 -0700

    add exec rights to scrips

--- hw2/hello.pl
+++ hw2/hello.pl
@@ -1,5 +1,23 @@
 #!/usr/bin/perl
+use CGI;
+$co = new CGI;  
+
 print "Content-type: text/html\n\n";
 print "Hello world from cgi which is not another name for perl although it is written in perl... anyways, the time is \n";
 $now_string = localtime();
 print "$now_string";
+
+
+my @colors = map { 
+  join "", map { sprintf "%02x", rand(255) } (0..2) 
+} (0..63);
+
+
+$co->start_html
+(
+    # -title=>'CGI Example', 
+    # -author=>'your Name', 
+    # -meta=>{'keywords'=>'CGI Perl'}, 
+    -BGCOLOR=>'black', 
+    # -LINK=>'red'
+)
