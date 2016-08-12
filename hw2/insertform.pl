abadb0b87184a42af55c53be7f38053c39f8b23f
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-12 10:10:17 -0700

    permissions

--- hw2/insertform.pl
+++ hw2/insertform.pl
@@ -6,13 +6,13 @@ my $q = CGI->new;
  
 
 
-# my %data;
-# $data{fullname} = $q->param('fullname');
-# $data{country} = $q->param('country');
-# $data{question} = $q->param('question');
-print $q->start_html(-title => 'A web form');
-# print $q->form;
-# if ($data{fullname} !~ /^[\s\w.-]+$/) {
-# 	print "Name must contain only alphanumerics, spaces, dots and dashes.";
-# 	exit;
-# }
+my %data;
+$data{fullname} = $q->param('fullname');
+$data{country} = $q->param('country');
+$data{question} = $q->param('question');
+ 
+print $q->form;
+if ($data{fullname} !~ /^[\s\w.-]+$/) {
+	print "Name must contain only alphanumerics, spaces, dots and dashes.";
+	exit;
+}
