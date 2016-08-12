0ca2edf8ff4111a5876201a16dd30900485e6303
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-12 08:40:20 -0700

    add hello cgi

--- hw2/hello.pl
+++ hw2/hello.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 print "Content-type: text/html\n\n";
-print "Hello world from cgi which is not another name for perl although it is written in perl... anyways, the time is \n";
+print "Hello world from CGI the time is \n";
 $now_string = localtime();
 print "$now_string";
 
