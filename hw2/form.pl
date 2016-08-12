6c49d8ca26cc7b0c259074abeedc80930e7a65ad
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-12 10:06:25 -0700

    permissions

--- hw2/form.pl
+++ hw2/form.pl
@@ -1,13 +1,19 @@
- #!/usr/bin/perl
-# use strict;
-use warnings;
+#!/usr/bin/perl
+
+use Data::Dumper;
 use CGI;
-# use CGI::Carp qw(fatalsToBrowser);
-
 my $q = CGI->new;
+ 
 
+
+
+# my %data;
+# $data{fullname} = $q->param('fullname');
+# $data{country} = $q->param('country');
+# $data{question} = $q->param('question');
+ 
 # print $q->header;
-
-# print $q->start_html(-title => 'A web form');
-
-print $q->start_form;
+# if ($data{fullname} !~ /^[\s\w.-]+$/) {
+# 	print "Name must contain only alphanumerics, spaces, dots and dashes.";
+# 	exit;
+# }
