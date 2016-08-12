090d36363158dafdd207d4c9d83fe2957fecd9ce
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-12 10:12:56 -0700

    permissions

--- hw2/insertform.pl
+++ hw2/insertform.pl
@@ -7,4 +7,11 @@ my $q = CGI->new;
 print $q->header;
 
 print $q->start_html(-title => 'A web form');
-#print $q->start_form;
+print $q->start_form(
+        -name    => 'main_form',
+        -method  => 'GET',
+        -enctype => &CGI::URL_ENCODED,
+        -onsubmit => 'return javascript:validation_function()',
+        -action => '/where/your/form/gets/sent', # Defaults to 
+                                                 # the current program
+);
