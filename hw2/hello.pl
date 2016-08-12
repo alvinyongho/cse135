0ae53c3a04a26344a64c475f9234dc6a0c44774c
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-12 08:51:57 -0700

    add hello cgi

--- hw2/hello.pl
+++ hw2/hello.pl
@@ -9,3 +9,24 @@ print "$now_string";
 $random_color = rand(0xffffff);
 
 # print $random_color;
+
+$template = Template->new;
+$template->process(\*DATA, {random_color => $random_color});
+
+__DATA__
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf=8">
+        <title>Demo</title>
+        <style>
+            body {
+                background-color: [% random_color | html %];
+            }
+
+       </style>
+    </head>
+    <body>
+        # <h1>Your content here</h1>
+    </body>
+</html>
