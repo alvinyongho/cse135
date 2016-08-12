2a1707c92833d2cd4dd0fc6bd5eeeef337d700c3
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-12 09:11:51 -0700

    add hello cgi

--- hw2/hello.pl
+++ hw2/hello.pl
@@ -1,12 +1,43 @@
 #!/usr/bin/perl
 use CGI qw/:standard :html3/;
-print "Content-type: text/html\n\n";
-print "Hello world from CGI the time is \n";
-$now_string = localtime();
-print "$now_string";
+# print "Content-type: text/html\n\n";
+# print "Hello world from CGI the time is \n";
+# $now_string = localtime();
+# print "$now_string";
 
-$random_color = rand(0xffffff);
+# $random_color = rand(0xffffff);
 
-# # print $random_color;
+# # # print $random_color;
 
-print body();
+# print body();
+
+
+
+$newStyle=<<END;
+<!-- 
+P.Tip {
+margin-right: 50pt;
+margin-left: 50pt;
+    color: red;
+}
+P.Alert {
+font-size: 30pt;
+    font-family: sans-serif;
+  color: red;
+}
+-->
+END
+print header();
+print start_html( -title=>'CGI with Style',
+	      -style=>{-src=>'http://www.capricorn.com/style/st1.css',
+	               -code=>$newStyle}
+             );
+print h1('CGI with Style'),
+      p({-class=>'Tip'},
+    "Better read the cascading style sheet spec before playing with this!"),
+      span({-style=>'color: magenta'},
+       "Look Mom, no hands!",
+       p(),
+       "Whooo wee!"
+       );
+print end_html;
