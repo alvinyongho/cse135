76147f1404abf9c266023027096bceb582ad60d9
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-12 08:49:59 -0700

    add hello cgi

--- hw2/hello.pl
+++ hw2/hello.pl
@@ -1,8 +1,11 @@
 #!/usr/bin/perl
+
+use Template;
 print "Content-type: text/html\n\n";
 print "Hello world from CGI the time is \n";
 $now_string = localtime();
 print "$now_string";
 
 $random_color = rand(0xffffff);
-print $random_color;
+
+# print $random_color;
