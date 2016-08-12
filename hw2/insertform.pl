dc68f996d6c7d40cbd4c2fde0bf23bb806aab3ce
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-12 10:11:18 -0700

    permissions

--- hw2/insertform.pl
+++ hw2/insertform.pl
@@ -5,14 +5,4 @@ use CGI;
 my $q = CGI->new;
  
 
-
-my %data;
-$data{fullname} = $q->param('fullname');
-$data{country} = $q->param('country');
-$data{question} = $q->param('question');
- 
-print $q->form;
-if ($data{fullname} !~ /^[\s\w.-]+$/) {
-	print "Name must contain only alphanumerics, spaces, dots and dashes.";
-	exit;
-}
+print $q->start_form;
