3e51253dc9992a3d9becdcbdedca938057cdbcf7
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-12 10:06:40 -0700

    permissions

--- hw2/form.pl
+++ hw2/form.pl
@@ -4,16 +4,13 @@ use Data::Dumper;
 use CGI;
 my $q = CGI->new;
  
-
-
-
-# my %data;
-# $data{fullname} = $q->param('fullname');
-# $data{country} = $q->param('country');
-# $data{question} = $q->param('question');
+my %data;
+$data{fullname} = $q->param('fullname');
+$data{country} = $q->param('country');
+$data{question} = $q->param('question');
  
-# print $q->header;
-# if ($data{fullname} !~ /^[\s\w.-]+$/) {
-# 	print "Name must contain only alphanumerics, spaces, dots and dashes.";
-# 	exit;
-# }
+print $q->header;
+if ($data{fullname} !~ /^[\s\w.-]+$/) {
+	print "Name must contain only alphanumerics, spaces, dots and dashes.";
+	exit;
+}
