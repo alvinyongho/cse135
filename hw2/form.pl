bef421d95c5ec31de0954d9e92c2c6b6374bb713
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-12 10:04:32 -0700

    permissions

---
+++ hw2/form.pl
@@ -0,0 +1,13 @@
+ #!/usr/bin/perl
+use strict;
+use warnings;
+use CGI;
+use CGI::Carp qw(fatalsToBrowser);
+
+my $q = new CGI;
+
+print $q->header;
+
+print $q->start_html(-title => 'A web form');
+
+print $q->start_form;
