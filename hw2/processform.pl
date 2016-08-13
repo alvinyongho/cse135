b35a43b73fa6f330aa8299a6a6ef54b925f9581d
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-13 08:17:05 -0700

    add form js

--- hw2/processform.pl
+++ hw2/processform.pl
@@ -4,12 +4,6 @@ use Data::Dumper;
 use CGI;
 my $q = CGI->new;
  
-$request_method = $ENV{'REQUEST_METHOD'};
-if ($request_method eq "GET") {
-    $query_string = $ENV{'QUERY_STRING'};
-} elsif ($request_method eq "POST") {
-    read (STDIN, $query_string, $ENV{'CONTENT_LENGTH'});
-} else {
-    &return_error (500, "Server Error",
-                        "Server uses unsupported method");
-}
+print $q->header;
+
+print $q->start_html(-title => 'A web form');
