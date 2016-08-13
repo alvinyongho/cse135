89288dec86a6cfbbe9322398d17117636bc05774
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-13 12:53:31 -0700

    add form js

--- hw2/sessionpage2.pl
+++ hw2/sessionpage2.pl
@@ -61,10 +61,10 @@ if ($request_type eq 'POST'){
 	# print "These are the parameters I received:\n\n";
 
 
-	printf "Your session ID is: %s\n", $s->id;
-	printf "This sessin is: %s\n", $s->is_new ? 'NEW': 'old';
-	printf "Stored session 'test' value: '%s'\n", $q->escapeHTML($s->param('test'));
-	printf "CGI Params: %s\n", join ', ', $q->param;
+	# printf "Your session ID is: %s\n", $s->id;
+	# printf "This sessin is: %s\n", $s->is_new ? 'NEW': 'old';
+	# printf "Stored session 'test' value: '%s'\n", $q->escapeHTML($s->param('test'));
+	# printf "CGI Params: %s\n", join ', ', $q->param;
 
 
 	print "Hello " . $q->param('name');
