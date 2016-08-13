aa77798565ea447d878b14a21259764447de833b
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-13 12:11:18 -0700

    add form js

---
+++ hw2/sessionpage1.pl
@@ -0,0 +1,80 @@
+#!/usr/bin/perl
+
+use Data::Dumper;
+use CGI qw/:standard :html3/;
+my $q = CGI->new;
+ 
+print $q->header;
+
+
+
+
+$newStyle=<<END;
+<!-- 
+
+Body {
+    background-color: #CCDDEE
+}
+P.Tip {
+margin-right: 50pt;
+margin-left: 50pt;
+    color: red;
+}
+P.Alert {
+font-size: 30pt;
+    font-family: sans-serif;
+  color: red;
+}
+-->
+END
+
+print $q->start_html(-title=>'Insert Form CGI',
+    -style=>{
+        -code=>$newStyle
+    }
+);
+
+
+print "<script src='form.js'    type='text/javascript'></script>\n";
+
+
+print $q->start_form(
+        -name    => 'main_form',
+        -id      => 'myForm',
+        -method  => 'POST',
+        -enctype => &CGI::URL_ENCODED,
+        -onsubmit => 'validate_session()',
+        -action => 'sessionpage2.pl', # Defaults to 
+                                                 # the current program
+);
+
+
+print "Name: ".$q->textfield(
+        -name      => 'name',
+        -value     => '',
+        -size      => 20,
+        -maxlength => 30,
+    );
+
+print $q->br;
+
+print "Favorite Color: ".$q->textfield(
+        -name      => 'color',
+        -value     => '',
+        -size      => 20,
+        -maxlength => 30,
+    );
+
+print $q->br;
+
+# print $q->popup_menu(-name=>'ins_method',
+# 				 -values=>['GET','POST'],
+# 				 -default=>'POST');
+
+# print $q->br;
+
+print $q->submit(-value=>'Submit');
+
+print $q->end_form;
+
+print $q->end_html;
---
+++ hw2/sessionpage2.pl
@@ -0,0 +1,78 @@
+#!/usr/bin/perl
+
+use Data::Dumper;
+use CGI qw/:standard :html3/;
+my $q = CGI->new;
+ 
+print $q->header;
+
+print $q->start_html(-title => 'Process form');
+
+my $request_type = $ENV{'REQUEST_METHOD'};
+my $query_to_parse = '';
+
+
+
+
+$newStyle=<<END;
+<!-- 
+
+Body {
+	background-color: #CCDDEE
+}
+P.Tip {
+margin-right: 50pt;
+margin-left: 50pt;
+    color: red;
+}
+P.Alert {
+font-size: 30pt;
+    font-family: sans-serif;
+  color: red;
+}
+-->
+END
+# print header();
+print start_html( 
+	-title=>'Process Form CGI',
+	-style=>
+	{
+	    -code=>$newStyle}
+);
+
+
+if ($request_type eq 'POST'){
+	# print 'THIS WAS A POST';
+	# read(STDIN, $request, $ENV{'CONTENT_LENGTH'}) || die "Couldn't get the query\n";
+	# print $request;
+
+	# print "These are the parameters I received:\n\n";
+
+
+	print "Hello " . $q->param('name');
+	print " nice to meet you!";
+
+
+} elsif ($request_type eq 'GET'){
+	# print 'IT WAS A GET';
+	# $request = $ENV{'QUERY_STRING'};
+	# print $request;
+
+	print "Uhh what are you doing!?";
+
+}
+
+
+
+
+
+
+# print h1('CGI with Style'),
+#       p({-class=>'Tip'},
+#     "Better read the cascading style sheet spec before playing with this!"),
+#       span({-style=>'color: magenta'},
+#        "Look Mom, no hands!",
+#        p(),
+#        "Whooo wee!"
+#        );
+print end_html;
