d3d10eea283fefd1ef501c685d8de4f7504ae416
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-13 12:24:46 -0700

    add form js

--- hw2/sessionpage2.pl
+++ hw2/sessionpage2.pl
@@ -68,7 +68,7 @@ if ($request_type eq 'POST'){
 	print " nice to meet you!";
 
 
-	print "<button id='clear_session'>Clear Session</button>";
+	print "<button id='clear_session' onclick='deleteSession()'>Clear Session</button>";
 
 
 } elsif ($request_type eq 'GET'){
