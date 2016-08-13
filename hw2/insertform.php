9b0d3ed5720c086e1022d9c4be8736f4afde2b53
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-13 10:11:04 -0700

    add form js

--- hw2/insertform.php
+++ hw2/insertform.php
@@ -21,9 +21,9 @@ echo "<body style='background-color:$background_color'>";
 
 <form id='myForm' name='main_form' method='POST' onsubmit='validate()' action ='processform.php'>
 
-	Name: <input type="text" name="name" value="<?php echo $name;?>">
-	<br/>
-	Favorite Color: <input type="text" name="color" value="<?php echo $color;?>">
+	<p>Name: <input type="text" name="name" value="<?php echo $name;?>"></p>
+	<p>Favorite Color: <input type="text" name="color" value="<?php echo $color;?>"></p>
+
 
 	<select name="ins_method">
 		<option value="POST">Post</option>
