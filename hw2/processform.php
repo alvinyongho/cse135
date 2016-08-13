f9ab907c98443a0ddca95d39acfc7f96345e0534
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-13 10:16:43 -0700

    add form js

--- hw2/processform.php
+++ hw2/processform.php
@@ -6,16 +6,21 @@
 <body>
 	
 <?php
-	echo '<p>Hello World from PHP on ' . date('Y-m-d h:i:s a') . '</p>';
+	
 
 
 	$method = $_SERVER['REQUEST_METHOD'];
 	switch($method){
 		case 'GET':
 			echo 'GET';
+
+			echo '<p>Hello $_GET['name'] from PHP on ' . date('Y-m-d h:i:s a') . '</p>';
+
 			break;
 		case 'POST':
 			echo 'POST';
+
+			echo '<p>Hello $_POST['name'] from PHP on ' . date('Y-m-d h:i:s a') . '</p>';
 			break;
 	}
 
