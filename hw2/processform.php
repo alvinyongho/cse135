f6fd709956496bd1ce9139fcddb7f4fa3a543fe9
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-13 09:42:08 -0700

    add form js

--- hw2/processform.php
+++ hw2/processform.php
@@ -8,16 +8,10 @@
 <?php
 	echo '<p>Hello World from PHP on ' . date('Y-m-d h:i:s a') . '</p>';
 
-	function random_color_part() {
-    return str_pad( dechex( mt_rand( 0, 255 ) ), 2, '0', STR_PAD_LEFT);
-}
 
-	function random_color() {
-    return random_color_part() . random_color_part() . random_color_part();
-}
+$background_color = '#CCDDEE';
 
-$rand_color = random_color();
-echo "<body style='background-color:#$rand_color'>";
+echo "<body style='background-color:$background_color'>";
 
 
 ?>
