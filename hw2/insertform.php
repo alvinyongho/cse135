a0915f1889336ecaa114a2ad38940f64b7f9d1c9
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-13 09:38:05 -0700

    add form js

---
+++ hw2/insertform.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+</head>
+
+<body>
+	
+<?php
+	echo '<p>Hello World from PHP on ' . date('Y-m-d h:i:s a') . '</p>';
+
+	function random_color_part() {
+    return str_pad( dechex( mt_rand( 0, 255 ) ), 2, '0', STR_PAD_LEFT);
+}
+
+	function random_color() {
+    return random_color_part() . random_color_part() . random_color_part();
+}
+
+$rand_color = random_color();
+echo "<body style='background-color:#$rand_color'>";
+
+
+?>
+
+<script src="form.js"></script>
+
+</body>
+
+</html>
---
+++ hw2/min/form-min.js
@@ -0,0 +1,1 @@
+function validate(){console.log("VALIDATING")}
