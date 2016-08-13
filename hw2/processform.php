fb6314dffaea4ef1b02758c7dde1d8364d5f2c01
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-13 10:14:42 -0700

    add form js

--- hw2/processform.php
+++ hw2/processform.php
@@ -9,6 +9,17 @@
 	echo '<p>Hello World from PHP on ' . date('Y-m-d h:i:s a') . '</p>';
 
 
+	$method = $_SERVER['REQUEST_METHOD'];
+	switch($method){
+		case 'GET':
+			echo 'GET';
+			break;
+		case 'POST':
+			echo 'POST';
+			break;
+	}
+
+
 $background_color = '#CCDDEE';
 
 echo "<body style='background-color:$background_color'>";
