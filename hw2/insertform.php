5d0e1434ee4a52818ddf3aa383945caf11c4db68
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-13 10:11:29 -0700

    add form js

--- hw2/insertform.php
+++ hw2/insertform.php
@@ -6,7 +6,7 @@
 <body>
 	
 <?php
-	echo '<p>Hello World from PHP on ' . date('Y-m-d h:i:s a') . '</p>';
+	// echo '<p>Hello World from PHP on ' . date('Y-m-d h:i:s a') . '</p>';
 
 
 $background_color = '#CCDDEE';
