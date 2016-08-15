179176d8433195cd85b1520bd78bfa78074de71a
Alvin Ho <alvinho@Alvins-MacBook-Pro-2.local>
2016-08-15 08:27:21 -0700

    update to random rwb

--- hw2/hello.php
+++ hw2/hello.php
@@ -8,15 +8,26 @@
 <?php
 	echo '<p>Hello World from PHP on ' . date('Y-m-d h:i:s a') . '</p>';
 
-	function random_color_part() {
-    return str_pad( dechex( mt_rand( 0, 255 ) ), 2, '0', STR_PAD_LEFT);
-}
 
-	function random_color() {
-    return random_color_part() . random_color_part() . random_color_part();
-}
+	$array = array("red", "white", "blue");
 
-$rand_color = random_color();
+
+
+	$k = array_rand($array);
+	$v = $array[$k];
+// 	function random_color_part() {
+//     return str_pad( dechex( mt_rand( 0, 255 ) ), 2, '0', STR_PAD_LEFT);
+// }
+
+// 	function random_color() {
+//     return random_color_part() . random_color_part() . random_color_part();
+// }
+
+// $rand_color = random_color();
+
+
+
+	$rand_color = $v;
 echo "<body style='background-color:#$rand_color'>";
 
 
